ce41e926a05afb4ad3097b792c286e74e7c3a12c
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-04-27 19:08:25 +0300

    Styled the navbar and header sections

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_movies
+from .request import get_movies, get_movie
 
 
 # Views
@@ -12,19 +12,21 @@ def index():
     # Getting popular movie
     popular_movies = get_movies('popular')
     upcoming_movie = get_movies('upcoming')
-    now_showing_movie = get_movies('now_playing')
+    now_playing = get_movies('now_playing')
     # print(popular_movies)
     
     title = 'Home - Welcome to The Best Movie Review Website'
-    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_playing = now_showing_movie)
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_playing = now_playing)
 
 
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+@app.route('/movie/<int:id>')
+def movie(id):
 
     '''
     View movie page function that returns the movie details page and its data
     '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
 
-    return render_template('movie.html', id = movie_id)
+    return render_template('movie.html', title = title, movie = movie)
